709a7a4a99b7346958e873c031fc980ca1f8de30
unreact-hatano <mizutama.hahaha@gmail.com>
2023-01-22 20:29:49 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import uuid from "react-uuid";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
+  const [activeNote, setActiveNote] = useState("");
+
+  useEffect(() => {
+    //ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
+  const onAddNote = () => {
+    // console.log("新しくノートが追加されました");
+    const newNote = {
+      id: uuid(),
+      title: "",
+      content: "",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  // console.log(notes);
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    //修正された新しいノートの配列を返す。
+    const updatedNotesArray = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    });
+
+    // console.log(updatedNote);
+    setNotes(updatedNotesArray);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+    </div>
+  );
+}
+
+export default App;
